a686e9838bd4c5b5eaa3a5a04db38eb3a8a65580
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-03-21 21:27:40 +0100

    Revision to History data + Added indexer property + Ability to add/remove history entries from interface layer

--- NOP.MMA/Core/Patients/Interfaces/IPregnancyHistory.cs
+++ NOP.MMA/Core/Patients/Interfaces/IPregnancyHistory.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace NOP.MMA.Core.Patients
 {
+    /// <summary>
+    /// Represents a collection of <see cref="IPregnancyHistoryEntry"/> <see langword="objects"/>
+    /// </summary>
     public interface IPregnancyHistory : IHistory <IPregnancyHistoryEntry>
     {
     }
