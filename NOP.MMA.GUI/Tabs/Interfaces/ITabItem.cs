0f7268f983655e6c3340d6c012080970de4ad381
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2021-04-19 19:59:40 +0200

    Implemented Tab System + Added ITabCollection interface + Added ITabItem interface + Added ITabItem<T> interface + Added NavTabPanel class + Added TabCollection class + Added PatientDataTab class + Added TabItem class + Added PregnancyJournalViewModel + Added TravelerJournalViewModel + Added sample code in MainWindow class + Added GUI setup for the base layout (NOTE: Legacy code included) ~ Fixed comma crash when building a Patient from storage ~ Fixed wrong path when setting storage path in PregnancyJournalRepo and TravelerJournalRepo

---
+++ NOP.MMA.GUI/Tabs/Interfaces/ITabItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace NOP.MMA.GUI.Tabs
+{
+    /// <summary>
+    /// Represents a tab <see langword="object"/>
+    /// </summary>
+    public interface ITabItem
+    {
+        int ID { get; }
+        /// <summary>
+        /// The text to display as the tabs header
+        /// </summary>
+        string Header { get; set; }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the tab is currently displated; Otherwise, if not, <see langword="false"/>
+        /// </summary>
+        bool IsVisible { get; }
+
+        /// <summary>
+        /// Construct the tab <see langword="object"/> based on a <paramref name="_headerArea"/> and a <paramref name="_contentArea"/>
+        /// </summary>
+        /// <param name="_headerArea">The area that defines the header for the <see cref="ITabItem"/></param>
+        /// <param name="_contentArea">The area that defines the content area for <see cref="ITabItem"/></param>
+        void Construct ( StackPanel _headerArea, Grid _contentArea );
+
+        /// <summary>
+        /// Show or hide the tab content
+        /// </summary>
+        /// <param name="_show">Whether or not to show or hide the tabs content</param>
+        void Show ( bool _show = true );
+
+        /// <summary>
+        /// Close the tab
+        /// </summary>
+        void Close ();
+    }
+}
---
+++ NOP.MMA.GUI/Tabs/PregnancyJournalPanel.cs
@@ -0,0 +1,144 @@
+﻿using NOP.MMA.Core.Journals;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace NOP.MMA.GUI.Tabs
+{
+    /// <summary>
+    /// Represents a <see cref="TabCollection"/> panel for <see cref="IPregnancyJournal"/> <see langword="objects"/>
+    /// </summary>
+    internal class PregnancyJournalPanel : TabCollection
+    {
+        /// <summary>
+        /// Initialize a new instance of type <see cref="PregnancyJournalPanel"/> where the <paramref name="_tabDisplay"/> and <paramref name="_contentArea"/> is defined
+        /// </summary>
+        /// <param name="_tabDisplay">The panel tabs are instantiated into</param>
+        /// <param name="_contentArea">The grid that contains the content for a given tab</param>
+        public PregnancyJournalPanel ( StackPanel _tabDisplay, Grid _contentArea ) : base ()
+        {
+            tabDisplay = _tabDisplay;
+            contentArea = _contentArea;
+        }
+
+        /// <summary>
+        /// The panel tabs are instantiated into
+        /// </summary>
+        private readonly StackPanel tabDisplay;
+        /// <summary>
+        /// The grid that contains the content for a given tab
+        /// </summary>
+        private readonly Grid contentArea;
+
+        /// <summary>
+        /// <inheritdoc/> and remove it from the <see cref="TabCollection"/>
+        /// </summary>
+        /// <param name="_tab">The tab to close</param>
+        /// <returns><inheritdoc/></returns>
+        public override bool CloseTab ( ITabItem _tab )
+        {
+            ITabItem item = Tabs.Find (item => item.ID == _tab.ID);
+
+            if ( item != null )
+            {
+                item.Close ();
+                return Tabs.Remove (item);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="_tab">The tab to minimize</param>
+        /// <returns><see langword="True"/> if the tab could be minimized; Otherwise, if not, <see langword="false"/></returns>
+        public override bool MinimizeTab ( ITabItem _tab )
+        {
+            if ( _tab != null )
+            {
+                _tab.Show (false);
+                return true;
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="_tab">The tab to open</param>
+        /// <returns><inheritdoc/></returns>
+        public override bool OpenTab ( ITabItem _tab )
+        {
+            if ( _tab != null )
+            {
+                _tab.Show ();
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Create a new tab and add it to the <see cref="TabCollection"/> without an associated <see cref="IPregnancyJournal"/> context
+        /// </summary>
+        /// <param name="_headerText">The text to display as header</param>
+        /// <param name="_showContent">Whether or not to display the tab after creation</param>
+        /// <returns>The newly created <see cref="ITabItem"/></returns>
+        public ITabItem CreateTab ( string _headerText, bool _showContent = true )
+        {
+            PregnancyJournalTab item = new PregnancyJournalTab (_headerText, _showContent);
+
+            #region On Click Behaviour
+            item.OnClick = ( o, e ) =>
+            {
+                ITabItem other = FindTab (item => item.IsVisible == true);
+                if ( other != null )
+                {
+                    MinimizeTab (other);
+                }
+
+                OpenTab (item);
+            };
+            #endregion
+
+            #region On Close Behaviour
+            item.OnCloseClick = ( o, e ) =>
+            {
+                CloseTab (item);
+
+                if ( FindTab (item => item.IsVisible == true) == null )
+                {
+                    if ( Tabs.Count > 0 && Tabs[ 0 ] != null )
+                    {
+                        OpenTab (Tabs[ 0 ]);
+                    }
+                }
+            };
+            #endregion
+
+            Tabs.Add (item);
+
+            item.Construct (tabDisplay, contentArea);
+
+            return item;
+        }
+
+        /// <summary>
+        /// Create a new tab and add it to the <see cref="TabCollection"/> with an associated <see cref="IPregnancyJournal"/> context
+        /// </summary>
+        /// <param name="_headerText">The text to display as header</param>
+        /// <param name="_journal">The associated <see cref="IPregnancyJournal"/> context</param>
+        /// <param name="_showContent">Whether or not to display the tab after creation</param>
+        /// <returns>The newly created <see cref="ITabItem"/></returns>
+        public ITabItem CreateTab ( string _headerText, IPregnancyJournal _journal, bool _showContent = true )
+        {
+            PregnancyJournalTab item = CreateTab (_headerText, _showContent) as PregnancyJournalTab;
+
+            item.Context = _journal;
+
+            return item;
+        }
+    }
+}
